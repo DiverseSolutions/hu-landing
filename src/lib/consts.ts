3a1ff3a81be50a43876710028ffe80af2fcd953b
bbattulga <battulga.dev@gmail.com>
2023-02-06 12:32:41 +0800

    add google analytics

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,3 +1,4 @@
 export const PASSWORD_MIN_REGEX = /.{8,}/
 export const PASSWORD_MIN_ERROR = 'Password too weak, minimum 8 characters, 1 letter 1 number'
 export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+import { GOOGLE_ANALYTICS_ID } from '@/lib/consts'
 
 export default function Document() {
   return (
@@ -7,6 +9,17 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {GOOGLE_ANALYTICS_ID ? <Script async strategy={'afterInteractive'} src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}></Script> : <></>}
+        {GOOGLE_ANALYTICS_ID ? (
+          <Script>
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ANALYTICS_ID}');
+          `}
+          </Script>
+        ) : (<></>)}
       </body>
     </Html>
   )

f3fdffca3674534a29a84fdfcb7fe70b34d935eb
bbattulga <battulga.dev@gmail.com>
2023-02-12 02:39:27 +0800

    try fix cicd build err

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { wrapper } from '../store/store';
 import { Provider } from 'react-redux'
-import Navbar from '@/components/navbar';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { ToastContainer } from 'react-toastify';
-import AuthLoader from '@/components/AuthLoader';
 import AuthFeature from '@/features/auth/AuthFeature';
 
 export default function App({ Component, pageProps, ...rest }: AppProps) {

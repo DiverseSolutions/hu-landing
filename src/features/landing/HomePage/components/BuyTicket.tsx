3d8fe97894b6f158e1c9f1609a5d1013fdfe3fc9
bbattulga <battulga.dev@gmail.com>
2023-02-13 21:33:14 +0800

    Purchase Early Bird Ticket

--- src/features/landing/HomePage/components/BuyTicket.tsx
+++ src/features/landing/HomePage/components/BuyTicket.tsx
@@ -5,9 +5,9 @@ type Props = {}
 
 function BuyTicket({ }: Props) {
     return (
-        <Link href="/early" className="flex w-full justify-center items-center p-1 bg-white cursor-pointer py-[14px] px-[18px]">
-            <span className='px-8 font-bold uppercase'>
-                Early Bird Ticket
+        <Link href="/early" className="flex w-full justify-center items-center p-1 bg-white cursor-pointer py-[14px] md:px-[18px]">
+            <span className='px-8 text-sm font-bold uppercase md:text-base'>
+                Purchase Early Bird Ticket
             </span>
         </Link>
     )

712ad394b95dfad532b04a0bd86322b5a7dc03d2
bbattulga <battulga.dev@gmail.com>
2023-02-13 13:15:34 +0800

    home navbar fixed

--- src/features/landing/HomePage/index.tsx
+++ src/features/landing/HomePage/index.tsx
@@ -30,7 +30,7 @@ function HomePage({ }: Props) {
         <>
             <MobileDrawer>
                 <div className="relative w-screen h-screen overflow-hidden bg-black">
-                    <div>
+                    <div className='fixed top-0 left-0 right-0 z-50'>
                         <HomeNavbar />
                     </div>
                     <div className='h-screen'>

550bd1af176826471bd5d0428fd7db544941de07
bbattulga <battulga.dev@gmail.com>
2023-02-26 22:42:36 +0800

    update idax

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withFonts = require('next-fonts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: process.env.DOTENV_IDAX_PROD ? true : false,
+  trailingSlash: process.env.DOTENV_IDAX ? true : false,
   reactStrictMode: true,
   experimental: {
     urlImports: [

225b70c1c8c1919e4ca67edc96b21a299d97dfdc
bbattulga <battulga.dev@gmail.com>
2023-03-01 16:39:52 +0800

    update social share img

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,7 @@ import type { AppProps } from 'next/app'
 import { wrapper } from '../store/store';
 import { Provider } from 'react-redux'
 import 'react-toastify/dist/ReactToastify.css';
-import HuOgImg from '@/assets/img/hu-og.jpg'
-
+import SocialShareImg from '@/assets/img/social-share.jpg'
 
 import { ToastContainer } from 'react-toastify';
 import AuthFeature from '@/features/auth/AuthFeature';
@@ -22,14 +21,14 @@ export default function App({ Component, pageProps, ...rest }: AppProps) {
         <meta property="og:description" content="Join the legendary band The HU for their first-ever virtual concert in the Metaverse!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/hu-favicon.ico" />
-        <meta property="og:image" content={`${APP_HOST_URL}${HuOgImg.src}`} />
-        <meta property="og:image:width" content={HuOgImg.width.toString()} />
-        <meta property="og:image:height" content={HuOgImg.height.toString()} />
+        <meta property="og:image" content={`${APP_HOST_URL}${SocialShareImg.src}`} />
+        <meta property="og:image:width" content={SocialShareImg.width.toString()} />
+        <meta property="og:image:height" content={SocialShareImg.height.toString()} />
         <meta property="og:image:type" content="image/jpeg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="The HU in the Metaverse" />
         <meta name="twitter:description" content="Join the legendary band The HU for their first-ever virtual concert in the Metaverse!" />
-        <meta name="twitter:image" content={`${APP_HOST_URL}${HuOgImg.src}`} />
+        <meta name="twitter:image" content={`${APP_HOST_URL}${SocialShareImg.src}`} />
       </Head>
       <Provider store={store}>
         <Component {...props.pageProps} />

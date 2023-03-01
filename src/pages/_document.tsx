57097310d08abf9b0272725e9837f87b019299d3
bbattulga <battulga.dev@gmail.com>
2023-03-01 13:41:48 +0800

    add facebook pixel tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,6 +28,25 @@ export default function Document() {
             <script src="https://mccdn.me/assets/js/widget.js" defer></script>
           </>
         ) : (<></>)}
+        {DEPLOYMENT_ENV === 'prod' ? (
+          <>
+            <script defer>
+              {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '748893529928638');
+          fbq('track', 'PageView');`}
+            </script>
+            <noscript><img height="1" width="1" style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=748893529928638&ev=PageView&noscript=1"
+            /></noscript>
+          </>
+        ) : (<></>)}
       </body>
     </Html>
   )

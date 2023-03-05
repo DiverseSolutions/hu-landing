8478d557156ebd0c3b86af2d74722fd04d82007f
bbattulga <battulga.dev@gmail.com>
2023-03-05 14:58:22 +0800

    update mobile navbar

--- src/components/drawer/MobileDrawer/index.tsx
+++ src/components/drawer/MobileDrawer/index.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import CopyRightSvg from '@/assets/svg/copyright.svg'
 import { MdArrowRightAlt } from 'react-icons/md'
 import Link from 'next/link'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { logoutSuccess } from '@/store/reducer/auth-reducer/actions'
-
+import { useSwipeable } from 'react-swipeable'
+import ArrowRightSvg from './img/arrow-right.svg'
 
 type Props = {
     children: React.ReactNode,
@@ -18,6 +19,31 @@ function MobileDrawer({ children, drawerContent }: Props) {
     const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
     const isAuthLoading = useAppSelector(state => state.auth.isLoading)
 
+    const swipeHandlers = useSwipeable({
+        onSwipedRight: () => {
+            handleClose()
+        },
+        onTap: () => {
+            console.log('onTap')
+        },
+        onSwiped: () => {
+            console.log('swiped')
+        },
+        onSwipeStart: () => {
+            console.log('swipe start')
+        },
+        swipeDuration: 1000,
+        trackMouse: true,
+        trackTouch: true,
+        preventScrollOnSwipe: true
+    })
+
+    const handleClose = () => {
+        if (drawerRef.current?.checked) {
+            drawerRef.current?.click()
+        }
+    }
+
     return (
         <div className="drawer drawer-end">
             <input ref={drawerRef} id="hu-drawer" type="checkbox" className="drawer-toggle" />
@@ -26,50 +52,53 @@ function MobileDrawer({ children, drawerContent }: Props) {
             </div>
             <div className="drawer-side">
                 <label htmlFor="hu-drawer" className="drawer-overlay"></label>
-                <div className='p-4 bg-primary w-[80%] flex h-full justify-between flex-col'>
+                <div {...swipeHandlers} className='p-4 bg-[#1A1A1A] w-[100%] flex h-full justify-between flex-col'>
                     {drawerContent ? drawerContent : (
-                        <div className='flex flex-col justify-between w-full h-full'>
+                        <div onClick={handleClose} className='flex flex-col justify-between w-full h-full'>
                             <div className="flex flex-col justify-center w-full">
-                                <div className='mt-0'>
-                                    {!isLoggedIn ? (
-                                        <div>
-                                            <ul className="w-full rounded-lg menu bg-secondary text-secondary-content">
-                                                <li><Link href="/auth?form=login" className='flex items-center justify-between w-full' rel="noreferrer" >Login <span><MdArrowRightAlt size={32} /></span></Link>
-                                                </li>
-                                                <li><Link href="/auth?form=signup" rel="noreferrer" className='flex items-center justify-between w-full'>Sign Up <span><MdArrowRightAlt size={32} /></span></Link>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    ) : (<></>)}
-                                </div>
-                                {!isAuthLoading && isLoggedIn ? (
-                                    <div className='mt-8'>
-                                        <ul className="w-full rounded-lg menu bg-secondary text-secondary-content">
-                                            <li><Link href="/profile" className='flex items-center justify-between w-full' target="_blank" rel="noreferrer" >Profile <span><MdArrowRightAlt size={32} /></span></Link>
-                                            </li>
-                                            <li><button onClick={() => {
-                                                dispatch(logoutSuccess())
-                                            }} className='flex items-center justify-between w-full'  >Logout <span><MdArrowRightAlt size={32} /></span></button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                ) : (<></>)}
-                                <div className='mt-8'>
-                                    <ul className="w-full rounded-lg menu bg-secondary text-secondary-content">
-                                        <li><a href="https://metaland.mn/en" className='flex items-center justify-between w-full' target="_blank" rel="noreferrer" >METALAND <span><MdArrowRightAlt size={32} /></span></a>
+                                <div>
+                                    <ul className="w-full space-y-4 rounded-lg menu">
+                                        <li>
+                                            <Link href="/" rel="noreferrer" className='flex items-center justify-between w-full text-xl font-bold text-white'>About us <span><ArrowRightSvg size={32} /></span></Link>
                                         </li>
-                                        <li><a href="https://ardcoin.com/" target="_blank" rel="noreferrer" className='flex items-center justify-between w-full'>ARDCOIN <span><MdArrowRightAlt size={32} /></span></a>
+                                        <li>
+                                            <a href="https://ardcoin.com/" target="_blank" rel="noreferrer" className='flex items-center justify-between w-full text-xl font-bold text-white'>ArdCoin</a>
                                         </li>
+                                        <li>
+                                            <a href="https://metaland.mn/en" className='flex items-center justify-between w-full text-xl font-bold text-white' target="_blank" rel="noreferrer" >Metaland</a>
+                                        </li>
+                                        <li>
+                                            <Link href="/terms-of-service/en" rel="noreferrer" className='flex items-center justify-between w-full text-xl font-bold text-white'>Help Center</Link>
+                                        </li>
+                                        {!isAuthLoading && isLoggedIn ? (
+                                            <li>
+                                                <Link href="/profile" rel="noreferrer" className='flex items-center justify-between w-full text-xl font-bold text-white'>My Profile</Link>
+                                            </li>
+                                        ) : (<></>)}
                                     </ul>
                                 </div>
                             </div>
-                            <div className="flex justify-center w-full align-bottom">
-                                <div className='flex justify-center w-full'>
-                                    <div className="flex items-center">
-                                        <div className="mr-1 "><CopyRightSvg /></div>
-                                        <p className="font-medium text-white uppercase">2023 The HU & ARD</p>
+                            <div className='mt-0'>
+                                {!isLoggedIn ? (
+                                    <div>
+                                        <ul className="w-full rounded-lg menu">
+                                            <li>
+                                                <Link href="/auth?form=signup" className='flex text-white items-center justify-center w-full bg-white bg-opacity-[0.04] text-sm rounded-xl font-bold' rel="noreferrer" >Signup</Link>
+                                            </li>
+                                            <li className='mt-2'>
+                                                <Link href="/auth?form=login" rel="noreferrer" className='flex font-bold text-sm text-black items-center justify-center w-full bg-white bg-opacity-[0.93] rounded-xl'>Log in</Link>
+                                            </li>
+                                        </ul>
                                     </div>
-                                </div>
+                                ) : (
+                                    <div>
+                                        <ul className="w-full rounded-lg menu">
+                                            <li onClick={() => dispatch(logoutSuccess())} className='mt-2'>
+                                                <span className='flex font-bold text-sm text-black items-center justify-center w-full bg-white bg-opacity-[0.93] rounded-xl'>Logout</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                )}
                             </div>
                         </div>
                     )}
--- src/features/landing/HomePageV2/index.tsx
+++ src/features/landing/HomePageV2/index.tsx
@@ -1,52 +1,59 @@
 import React from 'react'
 import BundlesSection from './components/BundlesSection'
 
-import BgImg from './img/bg.png'
+import HomeCoverImg from '@/assets/img/hu-home-cover.jpg'
 import ItemsSection from './components/ItemsSection'
 import SystemRequirementsTransparent from './components/SystemRequirementsTransparent'
-import BehindNavbar from '@/components/layout/BehindNavbar'
+import Navbar from '@/components/Navbar'
+import Link from 'next/link'
+import DesktopBehindNavbar from '@/components/layout/DesktopBehindNavbar'
 
 type Props = {}
 
 function HomePageV2({ }: Props) {
     return (
         <>
-            <BehindNavbar>
-                <div className="w-full mt-[72px] md:mt-0 relative h-screen bg-[#982626] overflow-hidden">
-                    <div className="absolute top-0 bottom-0 right-0 hidden md:block">
-                        <img src={BgImg.src} className="object-cover w-[44vw] h-auto mix-blend-darken" />
-                    </div>
-                    <div className="absolute inset-0">
-                        <div className="relative w-full h-full">
-                            <div className="flex justify-center w-full h-full">
-                                <div className="container h-full">
-                                    <div className="flex flex-col justify-between w-full h-full pb-16">
-                                        <div className="flex items-center justify-start md:w-[50%] h-full">
-                                            <div className="flex flex-col w-full">
-                                                <p className="text-xl text-center md:text-left md:text-[40px] md:leading-[44px] font-bold text-white">
-                                                    Be heard in every nation and every<br /> tongue wherever the sun rises
-                                                </p>
-                                                <div className="flex justify-center w-full mt-8 md:justify-start">
-                                                    <a href="#bundle-section" className="font-bold flex items-center text-xl text-center py-[14px] px-6 hover:bg-white hover:bg-opacity-1 text-black rounded-xl bg-white bg-opacity-[0.93]">
-                                                        Buy Ticket
-                                                    </a>
+            <div>
+                <DesktopBehindNavbar>
+                    <div className="w-full block md:block md:mt-0 relative md:h-screen bg-[#982626] md:overflow-hidden">
+                        <div className="absolute inset-0">
+                            <img src={HomeCoverImg.src} className="object-cover w-full h-full md:w-screen md:h-screen mix-blend-darken" />
+                            <div className="absolute inset-0" style={{
+                                background: `linear-gradient(90deg, #000000 19.27%, rgba(0, 0, 0, 0) 100%)`
+                            }}></div>
+                        </div>
+                        <div className="inset-0 md:absolute">
+                            <div className="relative w-full h-full">
+                                <div className="flex justify-center w-full h-full">
+                                    <div className="container h-full px-2 md:px-0">
+                                        <div className="flex flex-col justify-between w-full h-full pb-16">
+                                            <div className="flex mt-16 md:mt-0 items-center justify-start md:w-[50%] h-full">
+                                                <div className="flex flex-col w-full">
+                                                    <p className="text-[32px] leading-[36px] text-left md:text-left md:text-[40px] md:leading-[44px] font-bold text-white">
+                                                        Be heard in every nation and every<br /> tongue wherever the sun rises
+                                                    </p>
+                                                    <div className="flex w-full mt-8">
+                                                        <Link href="/" className="font-bold text-base flex justify-center items-center md:text-xl text-center py-2.5 md:py-[14px] px-4 md:px-6 hover:bg-white hover:bg-opacity-1 text-black rounded-xl bg-white bg-opacity-[0.93]">
+                                                            About The HU
+                                                        </Link>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div className='w-full px-2 md:px-0'>
-                                            <div className="w-full flex bg-white bg-opacity-[0.2] rounded-xl p-8">
-                                                <div className="flex flex-col justify-between w-full md:flex-row">
-                                                    <div className="flex flex-col">
-                                                        <p className="text-xl font-bold text-white">The Hu in the Metaverse</p>
-                                                        <p className="mt-4 text-base text-white max-w-[400px]">
-                                                            Be a part of history and join the legendary band HU for their first-ever virtual concert in the Metaverse!
-                                                        </p>
-                                                    </div>
-                                                    <div className="flex flex-col mt-4 md:mt-0">
-                                                        <SystemRequirementsTransparent />
-                                                        <div className="mt-4">
-                                                            <div className="bg-white cursor-pointer px-6 py-[14px] rounded-xl font-bold text-center">
-                                                                Download
+                                            <div className='w-full mt-16'>
+                                                <div className="w-full flex bg-white bg-opacity-[0.2] rounded-xl p-4 md:p-8">
+                                                    <div className="flex flex-col justify-between w-full md:flex-row">
+                                                        <div className="flex flex-col">
+                                                            <p className="text-base font-bold text-white md:text-xl">How to watch the concert?</p>
+                                                            <p className="mt-4 text-xs md:text-base text-white text-opacity-[0.65]">
+                                                                Please download and install this file on a computer that meets the system requirement and runs on Windows OS. By meeting the system requirements you will be able to enjoy the concert in high quality. The final file to watch the concert will be available for download on March 28, 2023. Demo file for installation and testing will be available on March 10, 2023.
+                                                            </p>
+                                                        </div>
+                                                        <div className="flex flex-col mt-4 md:mt-0 md:min-w-[300px] md:ml-6">
+                                                            <SystemRequirementsTransparent />
+                                                            <div className="mt-4">
+                                                                <div className="bg-white text-sm md:text-base cursor-pointer px-6 py-2.5 md:py-[14px] rounded-xl font-bold text-center">
+                                                                    Download
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -58,18 +65,18 @@ function HomePageV2({ }: Props) {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div id="bundle-section" className="pb-12">
-                    <div className="flex justify-center w-full px-4">
-                        <div className="mt-[64px] min-h-[50vh]">
-                            <BundlesSection />
+                    <div id="bundle-section">
+                        <div className="flex justify-center w-full px-4 mt-8">
+                            <div className="min-h-[50vh]">
+                                <BundlesSection />
+                            </div>
+                        </div>
+                        <div className="mt-[100px]">
+                            <ItemsSection />
                         </div>
                     </div>
-                    <div className="mt-[100px]">
-                        <ItemsSection />
-                    </div>
-                </div>
-            </BehindNavbar>
+                </DesktopBehindNavbar>
+            </div>
         </>
     )
 }

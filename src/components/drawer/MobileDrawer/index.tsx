78596b610d7db0ff1961d3f84af1ee73510ecb92
bbattulga <battulga.dev@gmail.com>
2023-03-09 12:47:51 +0800

    drawer login signup up

--- src/components/drawer/MobileDrawer/index.tsx
+++ src/components/drawer/MobileDrawer/index.tsx
@@ -76,30 +76,31 @@ function MobileDrawer({ children, drawerContent }: Props) {
                                             </li>
                                         ) : (<></>)}
                                     </ul>
+                                    <div className='mt-6'>
+                                        {!isLoggedIn ? (
+                                            <div>
+                                                <ul className="w-full rounded-lg menu">
+                                                    <li>
+                                                        <Link href="/auth?form=signup" className='flex text-white items-center justify-center w-full bg-white bg-opacity-[0.04] text-sm rounded-xl font-bold' rel="noreferrer" >Signup</Link>
+                                                    </li>
+                                                    <li className='mt-2'>
+                                                        <Link href="/auth?form=login" rel="noreferrer" className='flex font-bold text-sm text-black items-center justify-center w-full bg-white bg-opacity-[0.93] rounded-xl'>Log in</Link>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                <ul className="w-full rounded-lg menu">
+                                                    <li onClick={() => dispatch(logoutSuccess())} className='mt-2'>
+                                                        <span className='flex font-bold text-sm text-black items-center justify-center w-full bg-white bg-opacity-[0.93] rounded-xl'>Logout</span>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                            <div className='mt-0'>
-                                {!isLoggedIn ? (
-                                    <div>
-                                        <ul className="w-full rounded-lg menu">
-                                            <li>
-                                                <Link href="/auth?form=signup" className='flex text-white items-center justify-center w-full bg-white bg-opacity-[0.04] text-sm rounded-xl font-bold' rel="noreferrer" >Signup</Link>
-                                            </li>
-                                            <li className='mt-2'>
-                                                <Link href="/auth?form=login" rel="noreferrer" className='flex font-bold text-sm text-black items-center justify-center w-full bg-white bg-opacity-[0.93] rounded-xl'>Log in</Link>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                ) : (
-                                    <div>
-                                        <ul className="w-full rounded-lg menu">
-                                            <li onClick={() => dispatch(logoutSuccess())} className='mt-2'>
-                                                <span className='flex font-bold text-sm text-black items-center justify-center w-full bg-white bg-opacity-[0.93] rounded-xl'>Logout</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                )}
-                            </div>
+
                         </div>
                     )}
                 </div>

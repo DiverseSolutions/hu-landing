f60f28888e60d20fd2296cdafc9516bd729c389d
bbattulga <battulga.dev@gmail.com>
2023-03-13 11:11:25 +0800

    add text

--- src/features/landing/HomePageV2/components/BackgroundVideo/index.tsx
+++ src/features/landing/HomePageV2/components/BackgroundVideo/index.tsx
@@ -7,7 +7,7 @@ function BackgroundVideo({ }: Props) {
     const [isVideoLoading, setIsVideoLoading] = useState(true)
     return (
         <div className="absolute inset-0">
-            <video onLoadStart={() => setIsVideoLoading(true)} onLoadedData={() => setIsVideoLoading(false)} src="/hu-bg.mp4" autoPlay muted loop className='object-cover w-full h-full md:w-screen md:h-screen' />
+            <video onLoadedData={() => setIsVideoLoading(false)} src="/hu-bg.mp4" autoPlay muted loop className='object-cover w-full h-full md:w-screen md:h-screen' />
             <div className="absolute inset-0" style={{
                 background: `linear-gradient(90deg, #000000 19.27%, rgba(0, 0, 0, 0) 100%)`
             }}></div>

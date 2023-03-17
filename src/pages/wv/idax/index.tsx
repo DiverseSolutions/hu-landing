757db43e67bb6552db110495ec300eb99145f62f
bbattulga <battulga.dev@gmail.com>
2023-03-17 12:26:01 +0800

    update /wv/idax

--- src/pages/wv/idax/index.tsx
+++ src/pages/wv/idax/index.tsx
@@ -9,25 +9,14 @@ function WebViewIdax({ }: Props) {
 
     const router = useRouter()
     const [pageError, setPageError] = useState<string>()
-    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
-    const session = useAppSelector(state => state.auth.session)
-    const isAuthLoading = useAppSelector(state => state.auth.isLoading)
     const isLoading = !pageError
 
     useEffect(() => {
         if (!router.isReady) {
             return
         }
-        if (!isAuthLoading && !isLoggedIn) {
-            setPageError("Session Expired. Please try reload the page.")
-        }
-        if (!isAuthLoading && isLoggedIn && (session !== 'idax-wv')) {
-            setPageError("Invalid login session")
-        }
-        if (!isAuthLoading && (session === 'idax-wv') && isLoggedIn) {
-            router.push('/')
-        }
-    }, [session, isLoggedIn, isAuthLoading, isLoading, router.isReady])
+        router.push('/')
+    }, [router.isReady])
 
 
     if (pageError) {

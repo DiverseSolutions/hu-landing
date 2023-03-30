e78ce01e6f9729614c26f12d973333adc72cbadd
bbattulga <battulga.dev@gmail.com>
2023-03-30 20:36:00 +0800

    save cookie

--- src/components/video/DirectorCutVideo/index.tsx
+++ src/components/video/DirectorCutVideo/index.tsx
@@ -1,38 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useVideoJS } from "react-hook-videojs";
 import "video.js/dist/video-js.css";
 import { ArdArtHelperLiveResult } from "@/store/rtk-query/hux-ard-art/types";
 import "@videojs/themes/dist/fantasy/index.css";
+import Cookies from "js-cookie";
 
 type Props = {
     live: ArdArtHelperLiveResult
 }
 
-const DirectorCutVideoDemo = (props: Props) => {
+const DirectorCutVideoDemo = ({ live, ...props }: Props) => {
 
-    const videoUrl = props.live.url
-
+    const videoUrl = live.url
     const className = "w-full h-auto vjs-theme-fantasy video-js vjs-big-play-centered aspect-video";
     const { Video, player, ready } = useVideoJS(
         {
-            sources: [{ src: videoUrl, type: 'application/x-mpegURL' }],
+            sources: [{ src: videoUrl, type: 'application/x-mpegURL', }],
             controls: true,
             autoplay: 'muted',
             html5: {
                 hls: {
                     withCredentials: true,
                 }
-            }
+            },
         },
         className
     );
 
     useEffect(() => {
         if (ready && player) {
+            if (live.cookie?.length) {
+                live.cookie.forEach((c) => {
+                    Cookies.remove(c.Name, {
+                        domain: '.hu.rocks',
+                    })
+                    Cookies.set(c.Name, c.Value, {
+                        domain: '.hu.rocks',
+                    })
+                })
+            }
             (async () => {
                 try {
                     player.play()
-                    await player.requestFullscreen()
+                    player.requestFullscreen()
                 } catch (e) {
                     console.error('fullscreen request err')
                     console.error(e)
@@ -41,8 +51,8 @@ const DirectorCutVideoDemo = (props: Props) => {
         }
     }, [player])
 
-    if (ready) {
-        // Do something with the video.js player object.
+    if (!ready) {
+        return <></>
     }
     return (
         <>
